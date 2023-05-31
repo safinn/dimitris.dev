6e0a800c039b90c447988d116b1f7f17c6cbfad0
Dimitris <safinn@users.noreply.github.com>
2023-05-31 22:00:08 +0100

    update meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="author" content="Dimitris Karittevlis" />
         <Meta />
         <Links />
       </head>
