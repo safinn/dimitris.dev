96e369ada2a16d0b014c709f6bf9e502931acbdf
Dimitris <safinn@users.noreply.github.com>
2023-06-01 09:02:43 +0100

    basic index page content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,31 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div className="max-w-screen-sm mx-auto">
-      <h1>Index page</h1>
+      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100 mb-6">
+        Dimitris
+      </h1>
+      <p>
+        Software Engineer at{' '}
+        <a
+          className="text-zinc-900 dark:text-zinc-100"
+          href="https://riskledger.com"
+        >
+          Risk Ledger
+        </a>
+      </p>
+      <p>
+        Previously{' '}
+        <a className="text-zinc-900 dark:text-zinc-100" href="https://sky.com">
+          Sky
+        </a>{' '}
+        and{' '}
+        <a
+          className="text-zinc-900 dark:text-zinc-100"
+          href="https://rolls-roycemotorcars.com"
+        >
+          Rolls-Royce Motor Cars
+        </a>
+      </p>
     </div>
   )
 }
