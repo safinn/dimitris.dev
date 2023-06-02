e911a908e8b714cff21a7d566d08864878a82aaa
Dimitris Karittevlis <safinn@users.noreply.github.com>
2023-06-02 11:17:26 +0100

    use new dev server

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: 'cjs',
+  serverModuleFormat: 'esm',
   tailwind: true,
   future: {
+    unstable_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
