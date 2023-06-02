ad0ff1e515d376062d2bf6d0acbdce398b18caa5
Dimitris Karittevlis <safinn@users.noreply.github.com>
2023-06-02 11:07:43 +0100

    use express server for custom public caching

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverModuleFormat: 'cjs',
   tailwind: true,
   future: {
     v2_errorBoundary: true,
