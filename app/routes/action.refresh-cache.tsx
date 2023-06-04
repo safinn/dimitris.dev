c8c21d18ce097cdd848b51f1e35688f3eff65659
Dimitris <safinn@users.noreply.github.com>
2023-06-04 03:06:51 +0100

    parent a82dc943c35c63f08cbcfb26d613e7cf326e21a3 author Dimitris <safinn@users.noreply.github.com> 1685844411 +0100 committer Dimitris <safinn@users.noreply.github.com> 1685890142 +0100 gpgsig -----BEGIN SSH SIGNATURE-----  U1NIU0lHAAAAAQAAADMAAAALc3NoLWVkMjU1MTkAAAAgDWNnF8FHKqMOJFKMDWCn4K7Kps  SROMrszbp1Otcw0wcAAAADZ2l0AAAAAAAAAAZzaGE1MTIAAABTAAAAC3NzaC1lZDI1NTE5  AAAAQG2UzVTxglBYGI0p2LCKb2BGGb+41UrsFLd7qnR0/JCS/Ha8zSfwkd4FK3RpZg9wLT  pGayCu73uYLBfP8UXqzAk=  -----END SSH SIGNATURE-----
    refresh content workflow

---
+++ app/routes/action.refresh-cache.tsx
@@ -0,0 +1,105 @@
+import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
+import path from 'path'
+import invariant from 'tiny-invariant'
+import { cache } from '~/services/cache.server'
+import { getBlogMdxListItems, getMdxPage } from '~/services/mdx.server'
+
+type Body =
+  | { keys: Array<string>; commitSha?: string }
+  | { contentPaths: Array<string>; commitSha?: string }
+
+export type RefreshShaInfo = {
+  sha: string
+  date: string
+}
+
+export function isRefreshShaInfo(value: any): value is RefreshShaInfo {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'sha' in value &&
+    typeof value.sha === 'string' &&
+    'date' in value &&
+    typeof value.date === 'string'
+  )
+}
+
+export const commitShaKey = 'meta:last-refresh-commit-sha'
+
+export async function action({ request }: DataFunctionArgs) {
+  const { REFRESH_CACHE_SECRET } = process.env
+  invariant(REFRESH_CACHE_SECRET, 'REFRESH_CACHE_SECRET must be set')
+  // Everything in this function is fire and forget, so we don't need to await
+  // anything.
+  if (request.headers.get('auth') !== REFRESH_CACHE_SECRET) {
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  }
+
+  const body = (await request.json()) as Body
+
+  function setShaInCache() {
+    const { commitSha: sha } = body
+    if (sha) {
+      const value: RefreshShaInfo = { sha, date: new Date().toISOString() }
+      cache.set(commitShaKey, {
+        value,
+        metadata: {
+          createdTime: new Date().getTime(),
+          swr: Number.MAX_SAFE_INTEGER,
+          ttl: Number.MAX_SAFE_INTEGER,
+        },
+      })
+    }
+  }
+
+  if ('keys' in body && Array.isArray(body.keys)) {
+    for (const key of body.keys) {
+      void cache.delete(key)
+    }
+    setShaInCache()
+    return json({
+      message: 'Deleting cache keys',
+      keys: body.keys,
+      commitSha: body.commitSha,
+    })
+  }
+  if ('contentPaths' in body && Array.isArray(body.contentPaths)) {
+    const refreshingContentPaths = []
+    for (const contentPath of body.contentPaths) {
+      if (typeof contentPath !== 'string') {
+        continue
+      }
+
+      if (contentPath.startsWith('posts')) {
+        const [contentDir, dirOrFilename] = contentPath.split('/')
+        if (!contentDir || !dirOrFilename) {
+          continue
+        }
+        const slug = path.parse(dirOrFilename).name
+
+        refreshingContentPaths.push(contentPath)
+        void getMdxPage({ contentDir, slug }, { forceFresh: true })
+      }
+    }
+
+    // if any blog contentPaths were changed then let's update the dir list
+    // so it will appear on the blog page.
+    console.log(refreshingContentPaths)
+    if (refreshingContentPaths.some((p) => p.startsWith('posts'))) {
+      console.log('in getBlogMdxListItems')
+      void getBlogMdxListItems({
+        forceFresh: true,
+      })
+    }
+
+    setShaInCache()
+    return json({
+      message: 'Refreshing cache for content paths',
+      contentPaths: refreshingContentPaths,
+      commitSha: body.commitSha,
+    })
+  }
+  return json({ message: 'no action taken' }, { status: 400 })
+}
+
+export const loader = () => redirect('/', { status: 404 })
---
+++ app/routes/refresh-commit-sha[.]json.tsx
@@ -0,0 +1,28 @@
+import { json } from '@remix-run/node'
+import { cache } from '~/services/cache.server'
+import {
+  isRefreshShaInfo,
+  commitShaKey as refreshCacheCommitShaKey,
+  type RefreshShaInfo,
+} from './action.refresh-cache'
+
+export async function loader() {
+  const result = await cache.get(refreshCacheCommitShaKey)
+  if (!result) {
+    return json(null)
+  }
+
+  let value: RefreshShaInfo
+  try {
+    value = JSON.parse(result.value as any)
+    if (!isRefreshShaInfo(value)) {
+      throw new Error(`Invalid value: ${result.value}`)
+    }
+  } catch (error: unknown) {
+    console.error(`Error parsing commit sha from cache: ${error}`)
+    cache.delete(refreshCacheCommitShaKey)
+    return json(null)
+  }
+
+  return json(value)
+}
