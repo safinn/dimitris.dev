c60af9427a80fadf0e329a8424a49a5d9db8fc8d
Dimitris <safinn@users.noreply.github.com>
2023-06-07 00:24:14 +0100

    header anchor style

--- app/root.tsx
+++ app/root.tsx
@@ -68,7 +68,7 @@ function App() {
         <Links />
         <NonFlashOfWrongThemeEls ssrTheme={Boolean(data.theme)} />
       </head>
-      <body className="bg-zinc-200 dark:bg-zinc-950 text-zinc-700 dark:text-zinc-400 selection:bg-[#8884]">
+      <body className="bg-zinc-100 dark:bg-zinc-950 text-zinc-700 dark:text-zinc-400 selection:bg-[#8884]">
         <Header />
         <main className="py-8 px-6">
           <Outlet />
