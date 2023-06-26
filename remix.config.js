67854679544cbfc751e62107de880e1d900545d9
Dimitris Karittevlis <safinn@users.noreply.github.com>
2023-06-26 19:08:34 +0100

    update remix

--- remix.config.js
+++ remix.config.js
@@ -8,7 +8,7 @@ export default {
   serverModuleFormat: 'esm',
   tailwind: true,
   future: {
-    unstable_dev: true,
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
